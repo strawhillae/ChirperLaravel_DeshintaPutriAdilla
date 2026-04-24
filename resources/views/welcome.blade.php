bd06ca4338b580fac293a44f942e6a85c12288c3
deshinta putri adilla <deshinta.240170099@mhs.ac.id>
2026-04-24 20:11:13 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<x-layout>
+    <x-slot:title>
+        Home
+    </x-slot:title>
+
+    <div style="text-align: center; margin-top: 50px;">
+
+        <h1 style="font-weight: bold; font-size: 36px;">
+            Welcome to Chirper
+        </h1>
+
+        <p style="font-size: 18px;">
+            Halaman Pertama
+        </p>
+
+        <button style="
+            margin-top: 40px;
+            padding: 12px 24px;
+            font-size: 16px;
+            background-color: #3b82f6;
+            color: white;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+        ">
+            Jelajahi Sekarang →
+        </button>
+
+    </div>
+</x-layout>
